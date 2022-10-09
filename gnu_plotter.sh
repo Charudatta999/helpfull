f7fec3219f85fbdd9c5476fb35d33bd533029eeb
Charudatta Jadhav <cjawsome999@gmail.com>
2022-10-09 14:22:42 +0530

    Update gnu_plotter.sh

--- gnu_plotter.sh
+++ gnu_plotter.sh
@@ -1,24 +1,12 @@
 location_path=$1
 host_list=$2
 
-memory_repo=/root/Akshata/Excel/mem_details.csv
-cpu_repo=/root/Akshata/Excel/cpu_details.csv
-
-while read host_asset;do
-	Max_CPU=0
-	Min_CPU=0
-	Average_CPU=0
-	echo $host_asset
-	host_asset=`echo $host_asset| tr -d '\r'`
-    mkdir -p $location_path/$host_asset/plot-data
-	
-	cpu_filename=$location_path/$host_asset/plot-data/cpu_time_and_usage_edr.csv
-	read_byte_file_name=$location_path/$host_asset/plot-data/read_byte.csv
-	write_byte_file_name=$location_path/$host_asset/plot-data/write_byte.csv
-	resident_memory_filename=$location_path/$host_asset/plot-data/rss_time_and_usage_edr.csv
+memory_repo=
+cpu_repo=
 
 
-	nl -n ln resident_memory_filename | sed 's/,/ /g' > $location_path/$host_asset/plot-data/mem.dat
+
+	nl -n ln data.csv | sed 's/,/ /g' > $location_path/$host_asset/plot-data/mem.dat
 
 
 	gnuplot -e "set title 'usage graph';
@@ -31,5 +19,5 @@ while read host_asset;do
 	set terminal pngcairo size 15048,8440;
 	plot '$location_path/$host_asset/plot-data/mem.dat'  using 1:3:xtic(2) with boxes"
 	rm -rf $location_path/$host_asset/plot-data/mem.dat
-done < $host_list
+
 
