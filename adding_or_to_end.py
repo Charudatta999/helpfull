4b498f12f4e21a9a08f118a127eb28ef627e5799
Charudatta Jadhav <cjawsome999@gmail.com>
2022-10-10 09:36:27 +0530

    Update adding_or_to_end.py

--- adding_or_to_end.py
+++ adding_or_to_end.py
@@ -2,7 +2,7 @@ data=[]
 import math
 import sys
 file_name=sys.argv[2]
-with open('hostlist.txt', 'r') as f:
+with open(file_name, 'r') as f:
     dat=f.readlines()
 limit=100
 len_of_file=len(dat)
@@ -23,13 +23,4 @@ if len_of_file >limit:
         print("")
         prev=cur
         cur*=i
-# 800
-#     8 groups of 100
-#  1*100 = 0:100
-#  2*100 = 100:200
-#  3*100 = 200:300
-#  4*100 = 300:400
-#  5*100 = 400:500
-#  6*100 = 500:600
-#  7*100 = 600:700
-#  8*100 = 700:800
+
