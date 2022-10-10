b6d102ba7b2307d389765f9d1626ec70a78fd62d
Charudatta Jadhav <cjawsome999@gmail.com>
2022-10-10 10:59:06 +0530

    Update gnu_plotter.sh

--- gnu_plotter.sh
+++ gnu_plotter.sh
@@ -1,23 +1,82 @@
-location_path=$1
-host_list=$2
+#! /usr/bin/bash
 
-memory_repo=
-cpu_repo=
+FLAGS_PATTERN="i::o:r:h:"
+# i - input path, o - oputput path to save graphs in , h - help/usgae
+RES=""
+OUT_PATH=""
+IN_PATH=""
 
+IP_FLAG=/bin/false
+OP_FLAG=/bin/false
+RES_FLAG=/bin/false
 
-
-	nl -n ln data.csv | sed 's/,/ /g' > $location_path/$host_asset/plot-data/mem.dat
-
-
-	gnuplot -e "set title 'usage graph';
-	set term png; set xlabel 'time';set grid;
-	set ylabel 'usage'; set output 'rss_memory.png';
-	set boxwidth 0.2;
-	set style fill solid;
-	set xtics rotate;
-	set ytics font 'Times-Roman,50';set lmargin 50;
-	set terminal pngcairo size 15048,8440;
-	plot '$location_path/$host_asset/plot-data/mem.dat'  using 1:3:xtic(2) with boxes"
-	rm -rf $location_path/$host_asset/plot-data/mem.dat
+H_FLAG=/bin/false
 
 
+
+input_path=`echo $1`
+tmpfile=/tmp/`basename  $input_path | sed s/csv/dat/g `
+
+if [ -z "$2" ];then
+output_path=`echo $input_path | sed s/csv/png/g `
+else
+output_path=$2
+fi
+
+ nl -n ln $input_path | sed 's/,/ /g' > $tmpfile
+
+
+  gnuplot -e "set title 'usage graph';
+                 set term png; set xlabel 'time';set grid;
+                 set ylabel 'usage'; set output '$output_path';
+                 set boxwidth 0.2;
+                 set style fill solid;
+                 set xtics rotate;
+                 set ytics font 'Times-Roman,50';set lmargin 50;
+                 set terminal pngcairo size 15048,8440;
+                 plot '$tmpfile'  using 1:3:xtic(2) with boxes"
+
+rm -rf $tmpfile
+
+
+
+
+function parse_args()
+{
+    # This function parses the arguments passed to the script and sets the variables
+    
+    while getopts $FLAGS_PATTERN FLAG
+    do
+        case $FLAG in
+            i)
+                input_path=$OPTARG
+                IP_FLAG=/bin/true;;
+            
+            o)
+                OUT_PATH=$OPTARG
+                OP_FLAG=/bin/true;;
+            
+            r)
+                RES=$OPTARG
+                RES_FLAG=/bin/true;;
+            
+            h | *)
+            usage
+            exit 0;;
+        esac
+        
+    done
+}
+
+
+
+function main(){
+    parse_args "$@" #parsing arguments provided to script
+
+    if [[ -z $OP_FLAG ]];
+    then
+        OUT_PATH=`pwd`
+        echo " -o output Folder Path Not Specified, Using current directory: ${OUT_PATH}"
+    fi
+}
+main "$@"
